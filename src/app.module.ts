0720003eab97b95e8b8a9f51e45fb266c31d46c2
Niriksh Bharti <nirikshoxicated@gmail.com>
2026-04-05 01:24:10 +0530

    Learned Middlware logged each request, learned how express is invovled with nest js and how it uses it

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuditMiddleware } from './audit.middleware';
 
 @Module({
   imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(AuditMiddleware)
+      .forRoutes('*');
+  }
+
+}
---
+++ src/audit.middleware.ts
@@ -0,0 +1,11 @@
+import { Injectable, NestMiddleware } from "@nestjs/common";
+import { Request, Response, NextFunction } from "express"
+
+@Injectable()
+export class AuditMiddleware implements NestMiddleware {
+    use(req: Request, res: Response, next: NextFunction) {
+        console.log('Another plane just hit the tower', req.method, res);
+        next();
+
+    }
+}
